9b1f28a84274cccc2b5fb884077771350bf22375
Andrei Amatuni <andrei.amatuni@gmail.com>
2017-05-02 15:02:56 -0400

    fix dependency packaging

--- tests/testthat/test-collecting.R
+++ tests/testthat/test-collecting.R
@@ -1,5 +1,4 @@
 library(seedlings)
-library(tidyverse)
 
 audiomonths <- concat_month_bl("../basiclevel_dataprep/all_bl/", "output/", "audio")
 audiostats <- concat_all_bl(audiomonths, "all_audio.csv")
@@ -11,5 +10,7 @@ all_bl <- join_full_audio_video(audiostats, videostats, "allofthem.csv")
 
 all_bl <- add_chi_noun_onset(all_bl)
 
-count_experimentwords(all_bl)
-count_mot_fat(all_bl)
+# count_experimentwords(all_bl)
+# count_mot_fat(all_bl)
+
+all_bl_agg <- big_aggregate(all_bl)
